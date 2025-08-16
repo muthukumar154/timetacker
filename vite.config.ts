93305c7cb03290741c5ac8674be7d7912f30d872
muthukumar154 <muthukumarcse8@gmail.com>
2025-08-16 16:11:48 +0530

    deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base :"/timetracker/",
+  base :"/timetacker/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
